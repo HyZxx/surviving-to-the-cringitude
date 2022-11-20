de3d81be3d48f79bcf98a35026110d40cdb3d43d
luckil <lr.hoarau@gmail.com>
2022-11-20 17:38:52 +0400

    Création du monstre

---
+++ monster.py
@@ -0,0 +1,11 @@
+import pygame
+
+#Créer le monstre
+class Monster(pygame.srpite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.health = 100
+        self.max_health = 100
+        self.attack = 5
+        self.image = pygame.image.load('assets/mummy.png')
+        self.rect = self.image.get_rect()
