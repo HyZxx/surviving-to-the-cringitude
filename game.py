a4bf62b01047e8b9f3e164d0216574ff7a159af9
luckil <lr.hoarau@gmail.com>
2022-11-20 17:42:27 +0400

    apparition du monstre

--- game.py
+++ game.py
@@ -1,9 +1,17 @@
 import pygame
 from player import Player
+from monster import Monster
 
 #Créer la representation du jeu
 class Game:
     def __init__(self):
         #génerer le joueur
         self.player = Player()
+        #groupe de monstre
+        self.all_monsters = pygame.sprite.Group()
         self.pressed = {}
+        self.spawn_monster
+
+    def spawn_monster(self):
+        monster = Monster()
+        self.all_monsters.add(monster)
